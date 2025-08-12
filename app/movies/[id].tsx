51625ea9168863a456f9486c2c2ca5332a80b414
Christadrian Sanya <contact@christadriansanya.me>
2025-08-12 21:19:59 +0300

    feat: migrate to NativeWind and refactor asset structure
    - Replace theme system with NativeWind and TailwindCSS
    - Add new tab navigation structure with custom tab bar
    - Update asset organization and remove unused images
    - Add type definitions for images and interfaces
    - Configure babel and metro for NativeWind support

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MovieDetail = () => {
+    return (
+        <View>
+            <Text>Movie Detail</Text>
+        </View>
+    )
+}
+
+export default MovieDetail
